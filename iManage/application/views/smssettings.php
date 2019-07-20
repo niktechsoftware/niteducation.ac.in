4218b996ed23d7fbcc20e5c81afaa9f000f91763
imamityadav <amityadav2712@gmail.com>
2019-07-20 13:06:52 +0530

    add

--- iManage/application/views/smssettings.php
+++ iManage/application/views/smssettings.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
     <title>SMS Setting</title>
   </head>
   <body>
@@ -39,8 +43,24 @@
   <input type="checkbox" value="NITEDU">NITEDU
       </div></div>
       <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-6">
+      <input id="toggle-trigger" type="checkbox" checked data-toggle="toggle">
       </div>
       </div>
   </body>
+  <script>
+  //If you want to change it dynamically
+  function toggleOn() {
+    $('#toggle-trigger').bootstrapToggle('on')
+  }
+  function toggleOff() {
+    $('#toggle-trigger').bootstrapToggle('off')  
+  }
+  //if you want get value
+  function getValue()
+  {
+   var value=$('#toggle-trigger').bootstrapToggle().prop('checked');
+   console.log(value);
+  }
+</script>
 </html>
